55d23b0ed9f72db267b5ec6e9f0d9bfd21c9e505
CellineJacintaMwaura <jacinta.mwaura@student.moringaschool.com>
2026-08-18 01:10:28 +0300

    Routes

--- Nairobi-House-Hunt/src/App.jsx
+++ Nairobi-House-Hunt/src/App.jsx
@@ -1,15 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
+import About from "./pages/About"
 
 function App(){
   return(
-    <BrowserRouter>
+    <>
+    <header>
+      <h1>Nairobi House Hunt</h1>
+      <Navbar />
+    </header>
+
     <Routes>
       <Route path ="/" element={<Home />} />
       <Route path="/properties" element={<Properties />} />
+      <Route path="/about" element={<About />}/>
     </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
