61045a4215215c07815af317cf265648213607a1
CellineJacintaMwaura <jacinta.mwaura@student.moringaschool.com>
2026-08-17 19:12:47 +0300

    Confirming API Functionality

---
+++ Nairobi-House-Hunt/src/services/api.js
@@ -0,0 +1,12 @@
+const API_URL = "https://api.apify.com/v2/actors/nomad-agent~buyrentkenya-scraper/runs/last/dataset/items";
+const API_TOKEN = import.meta.env.VITE_APIFY_TOKEN; 
+
+export async function fetchProperties(){
+    const response = await fetch(
+        `${API_URL}?token=${API_TOKEN}&limit=20`
+    );
+    if (!response.ok){
+        throw new Error("Failed to fetch properties");
+    }
+    return response.json();
+}
