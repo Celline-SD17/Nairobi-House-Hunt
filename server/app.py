7ac933d7304c0691a91dcc298acce72b41af9d40
CellineJacintaMwaura <jacinta.mwaura@student.moringaschool.com>
2026-08-24 14:28:17 +0300

    Flask Backend Setup and PostgreSQL setup

---
+++ server/app.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def home():
+    return {"message": "Nairobi House Hunt API is running!"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
