e1a4b029b8276e358d773cc78d04fb04324e2993
CellineJacintaMwaura <jacinta.mwaura@student.moringaschool.com>
2026-08-24 15:48:22 +0300

    Relationships between users, properties, and favorites

--- server/models.py
+++ server/models.py
@@ -9,6 +9,10 @@ class User(db.Model):
     password_hash = db.Column (db.string(128), nullable=False)
     role = db.Column(db.String(20), nullable=False)
 
+
+    properties = db.relationship("Property", back_populates="landlord", cascade="all, delete-orphan")
+    favorites = db.relationship("Favorite", back_populates="user", cascade="all, delete-orphan")
+
     def __repr__(self):
         return f"<User {self.username}>"
 
@@ -24,14 +28,33 @@ class Property(db.Model):
     location = db.Column(db.String(200), nullable=False)
     county = db.Column(db.String(100), default="Nairobi")
     city = db.Column(db.String(100), default="Nairobi")
-    area = db.Column(db.STring(100), nullable=False)
+    area = db.Column(db.String(100), nullable=False)
     bedrooms = db.Column(db.Integer, nullable=False)
     bathrooms = db.Column(db.Integer, nullable=False)
     property_type = db.Column(db.String(100), nullable=False)
-    description = db.COlumn(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    landlord_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    landlord = db.relationship("User", back_populates="properties")
+    favorites = db.relationship("Favorite", back_populates="property", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"<Property {self.title}>"
+
+
+
 
 class Favorite(db.Model):
     __tablename__ = "favorites"
 
     id = db.Column(db.Integer, primary_key=True)
     notes = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.integer, db.ForeignKey("users.id"), nullable=False)
+    property_id =db.Column(db.Integer, db.ForeignKey("properties.id"), nullable=False)
+
+    user = db.relationship("User", back_populates="favorites")
+    property = db.relationship("property", back_populates="favorites")
+    __table_args__ = (db.UniqueConstraint("user_id", "property_id", name="unique_user_property_favorite"))
+
+    def __repr__(self):
+        return f"<Favorite User {self.user_id} Property{self.property_id}>"
