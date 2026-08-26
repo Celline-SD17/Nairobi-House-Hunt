9f5b40d789982cac366fd60fcb8aac691aaf80b3
CellineJacintaMwaura <jacinta.mwaura@student.moringaschool.com>
2026-08-26 16:06:41 +0300

    Changes in View Listing

--- Nairobi-House-Hunt/src/components/PropertyCard.jsx
+++ Nairobi-House-Hunt/src/components/PropertyCard.jsx
@@ -1,4 +1,22 @@
+
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 function PropertyCard({ property, isFavorite, onToggleFavorite }){
+    const [favoriteError, setFavoriteError] = useState("");
+    const navigate = useNavigate();
+    const handleFavoriteClick = async () => {
+        const result = await onToggleFavorite(property.id);
+        if (result === "login required") {
+            setFavoriteError("Please log in to add properties to your favorites.");
+            setTimeout(() => {
+                setFavoriteError("");
+            }, 1200);
+        }
+        else {
+            setFavoriteError("");
+        }
+    }; 
     return (
         <article className="property-card"> 
             <h2>{property.title}</h2>
@@ -8,13 +26,19 @@ function PropertyCard({ property, isFavorite, onToggleFavorite }){
             {property.bathrooms !== null && (
                 <p>{property.bathrooms} Bathrooms</p>
             )}
-            <p>{property.propertyType}</p>
-            <a href={property.url} target="_blank" rel="noreferrer" className="view-listing">
-                View Listing
-            </a>
-            <button onClick={() => onToggleFavorite(property.id)}>
+            <p>{property.property_type}</p>
+            <Link to={`/properties/${property.id}`}>View Listing </Link>
+            <button type="button" onClick={handleFavoriteClick}>
                 {isFavorite ? "❤️ Remove from Favorites" : "🤍 Add to Favorites"}
             </button>
+            {favoriteError && (
+                <div className="favorite-error">
+                    <p>{favoriteError}</p>
+                    <button onClick={() => navigate("/login")}>
+                        Log In
+                    </button>
+                </div>
+            )}
         </article>
     );
 }
