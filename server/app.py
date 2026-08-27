627068fc2c0dcc6f0f82ba20f0cce3ddbc57c50d
CellineJacintaMwaura <jacinta.mwaura@student.moringaschool.com>
2026-08-27 20:23:04 +0300

    Deployment link

--- server/app.py
+++ server/app.py
@@ -23,7 +23,10 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 CORS(
     app, 
-    origins=["http://localhost:5173"],        
+    origins=[
+        "http://localhost:5173",
+        "https://nairobi-house-hunt-api.onrender.com/"
+        ],        
     supports_credentials=True
 )
 
